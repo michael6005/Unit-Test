ca9db5ed57d229bec43d8e05975ee92397b352ab
Jonathan Dagan <jonathandagan12@gmail.com>
2021-10-30 18:58:00 +0300

    Card class fixes

--- Card.py
+++ Card.py
@@ -1,21 +1,37 @@
+from enum import Enum, IntEnum
+
+class Suit(IntEnum):
+    Diamond = 1
+    Spade = 2
+    Heart = 3
+    Club = 4
+
+class Value(IntEnum):
+    ace = 1
+    two = 2
+    three = 3
+    four = 4
+    five = 5
+    six = 6
+    seven = 7
+    eight = 8
+    nine = 9
+    ten = 10
+    jack = 11
+    queen = 12
+    king = 13
+
 class Card:
-# A class that describes a card and compares cards by value and suit
-    def __init__(self,suit,value):
+    # A class that describes a card and compares cards by value and suit
+    def __init__(self,suit: Suit,value: Value):
         self.value = value
         self.suit = suit
-        self.suits_names = ['Diamond','Spade','Heart','Club']
-        self.value_names = [None, 'Ace', '2', '3', '4', '5', '6', '7','8', '9', '10', 'Jack', 'Queen', 'King']
 
     def __repr__(self):
-        return '%s : %s' % (self.value_names[self.value], self.suits_names[self.suit])
-    def __gt__(self, other):
-        # compare values
-        if self.value > other.value:
-            return True
-        if self.value < other.value:
-            return False
-        # if the values are the same, compare the suits
-        if self.suit > other.suit:
-            return True
-        if self.suit < other.suit:
-            return False
+        return f'{self.value}, {self.suit}'
+    
+    def __gt__(self, other: Card):     
+        if self.value == other.value:
+            return self.suit > other.suit
+        else:
+            self.value > other.value
