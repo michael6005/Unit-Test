d1fc2180f76cd2fc9e3495fb7c4bbbce1c04c4cd
Jonathan Dagan <jonathandagan12@gmail.com>
2021-10-30 18:34:35 +0300

    delete duplicates

--- Card.py
+++ Card.py
@@ -1,21 +1,21 @@
 class Card:
-    def __init__(self, value, suit):
+# A class that describes a card and compares cards by value and suit
+    def __init__(self,suit,value):
         self.value = value
         self.suit = suit
-        self.suits = {1:"Diamond",2:"Spade",3:"Heart",4:"Club"}
+        self.suits_names = ['Diamond','Spade','Heart','Club']
+        self.value_names = [None, 'Ace', '2', '3', '4', '5', '6', '7','8', '9', '10', 'Jack', 'Queen', 'King']
+
+    def __repr__(self):
+        return '%s : %s' % (self.value_names[self.value], self.suits_names[self.suit])
     def __gt__(self, other):
+        # compare values
         if self.value > other.value:
             return True
-        elif self.value == other.value and self.suit > other.suit:
-            return True
-        else:
+        if self.value < other.value:
             return False
-    def __repr__(self):
-        return f"{self.value},{self.suits[self.suit]}"
-#if __name__=="main":
-# Card1=Card(13,4)
-# Card2=Card(13,1)
-# print(Card1<Card2)
-# print(Card1==Card2)
-# print(Card1>Card2)
-# suits={"Diamond":1,"Spade":2,"Heart":3,"Club":4}
+        # if the values are the same, compare the suits
+        if self.suit > other.suit:
+            return True
+        if self.suit < other.suit:
+            return False
